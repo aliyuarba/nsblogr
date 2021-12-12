e3c72192e774a72f288e554414f4f97f4b9de917
aliyuarba <arba313@gmail.com>
2021-12-12 15:30:58 +0700

    fix bug

--- js/app.js
+++
@@ -1,8 +0,0 @@
-$('.main-menu').click(function(){
-  $(this).toggleClass('active');
-  $(this).siblings().removeClass('active');
-})
-$('.btnmenu').click(function(){
-  $(this).toggleClass('active');
-  $('.navbar').toggleClass('active');
-})
